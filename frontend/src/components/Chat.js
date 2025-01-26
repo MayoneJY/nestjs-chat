7362f43f786ca50a4a652f6ac10938012ba76f0a
MayoneJY <mayone6063@kakao.com>
2025-01-26 20:13:44 +0900

    feat: 위로 스크롤 했을 때 더 많은 메시지 불러오는 기능 추가.

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { use } from 'react';
 import { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import { useParams } from 'react-router-dom';
@@ -11,6 +11,11 @@ const Chat = () => {
     const [input, setInput] = useState('');
     const { name } = useParams();
     const [chatroomId, setChatroomId] = useState('');
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [scroll, setScroll] = useState(false);
+    const [isEnd, setIsEnd] = useState(false);
+    const [scrollLength, setScrollLength] = useState(0);
 
     const chatRef = useRef();
 
@@ -26,13 +31,56 @@ const Chat = () => {
         overflowY: 'scroll',
     };
 
+    useEffect(() => {
+        if (loading || isEnd || !scroll) return;
+        // 채팅 내역이 요소 크기 보다 작을 때
+        if(chatRef.current.scrollHeight < 400) return;
+        setLoading(true);
+        setPage((prev) => prev + 1);
+        console.log(chatroomId, page + 1);
+        axios.get(`http://localhost:4000/chat?chatroomId=${chatroomId}&page=${page + 1}`)
+        .then((res) => {
+            if (res.data.length < 50) setIsEnd(true);
+            setScrollLength(res.data.length);
+            console.log(res.data.length);
+            setMessages((prev) => [...res.data, ...prev]);
+        })
+        .catch((res) => {
+            alert('error');
+        });
+
+        
+        // 스크롤
+
+        
+
+        setLoading(false);
+        setScroll(false);
+        
+    }, [scroll]);
+
+    useEffect(() => {
+        if (scrollLength === 0) return;
+        chatRef.current.scrollTo({ top: chatRef.current.querySelector(`[data-key="${scrollLength}"]`).offsetTop, behavior: "auto" });
+
+        setScrollLength(0);
+
+    }, [scrollLength]);
+
+    // 스크롤 이벤트 처리
+    const handleScroll = () => {
+        if (chatRef.current.scrollTop === 0) {
+            setScroll(true);
+        }
+    };
+
     useEffect(async () => {
         let chatroomIdTemp = '';
         // api call
         await axios.get('http://localhost:4000/chatroom/' + name)
         .then((res) => {
             chatroomIdTemp = res.data.chatroomId;
-            setChatroomId(res.data.chatroomId);
+            setChatroomId(chatroomIdTemp);
         })
         .catch((res) => {
             window.location.href = '/';
@@ -54,10 +102,16 @@ const Chat = () => {
         };
 
         window.addEventListener("beforeunload", handleBeforeUnload);
+
+        const scrollContainer = chatRef.current;
+        scrollContainer.addEventListener("scroll", handleScroll);
+
+
         setTimeout(() => chatRef.current.scrollTo({ top: chatRef.current.scrollHeight, behavior: "smooth" }), 100);
         return () => {
             socket.off('sendMessage');
             window.removeEventListener("beforeunload", handleBeforeUnload);
+            scrollContainer.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
@@ -67,13 +121,13 @@ const Chat = () => {
         setInput('');
         // 스크롤 내림
         setTimeout(() => chatRef.current.scrollTo({ top: chatRef.current.scrollHeight, behavior: "smooth" }), 100);
-        
     };
     return (
         <div>
+            <h1>{page}</h1>
             <div style={chatStyle} ref={chatRef}>
                 {messages.map((msg, idx) => (
-                <div key={idx}>
+                <div key={idx} data-key={idx}>
                     <strong>{msg.user}:</strong> {msg.message}
                 </div>
                 ))}
